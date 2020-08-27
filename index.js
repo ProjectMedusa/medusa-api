ccb4114ad421f74bc78e46ddca969fd230bef1a4
AidasPa <aidas0330@gmail.com>
2020-08-27 21:35:57 +0300

    airport api

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/airports/:icao', (req, res) => {
-  res.send('Hello World!')
+app.get('/api/airports/:icao', async (req, res) => {
+  const icao = req.params.icao;
+  if(!require('fs').existsSync(`./data/${icao}.json`)) {
+    res.sendStatus(404);
+  } else {
+    const data = await require('fs').promises.readFile(`./data/${icao}.json`, 'utf-8')
+    res.json(JSON.parse(data));
+  }
 })
 
 app.listen(port, () => {
